e5ba6f1058e5124e3a2e3cefbe66b3969d0eb558
Felipe de Castro <felipecastrotc@gmail.com>
2020-04-07 09:14:38 -0300

    updated 2020-04-06

--- graph_codes.py
+++ graph_codes.py
@@ -1,11 +1,19 @@
+from plotly.subplots import make_subplots
 import plotly.graph_objects as go
 from matplotlib import cm
 import numpy as np
-import panads as pd
+import pandas as pd
+
+
+df = pd.read_hdf("./data/covid19.h5", "covid19_data")
+
+# Available data
+ctry_arr = df.index.levels[0].values
+dates_arr = pd.to_datetime(df.columns[3:-1].values)
+sts_arr = np.unique(df.index.levels[1].values)
 
 
 # Choropleth chart
-
 n_color = 6
 cmap = cm.get_cmap("Blues")
 
@@ -17,13 +25,12 @@ for i in range(n_color):
     colorscale += [[10 ** (-(n_color - i - 1)), color]]
 colorscale[0][0] = 0.0
 
-
 ix = pd.IndexSlice
 slc_st = ix[:, "deaths"]
 col_date = dates_arr[-1].strftime("%Y-%m-%d")
 
-iso = df.loc[slc_st, "ISO"].groupby(CTRY_K).min()
-cases = df.loc[slc_st, col_date].groupby(CTRY_K).sum()
+iso = df.loc[slc_st, "ISO"].groupby('Country/Region').min()
+cases = df.loc[slc_st, col_date].groupby('Country/Region').sum()
 
 df.loc[slc_st, col_date]
 ctry = df.index.unique(0)
@@ -48,12 +55,12 @@ fig.update_layout(
     annotations=[dict(x=0.55, y=0.1, xref="paper", yref="paper",)],
 )
 
-
 fig.show()
 
 dct = fig.to_dict()
 
 # Pie Chart
+
 slc_st = ix[:, "deaths"]
 col_date = dates_arr[-1].strftime("%Y-%m-%d")
 cases = df.loc[:, col_date].groupby("status").sum()
@@ -70,3 +77,42 @@ fig = go.Figure(
     )
 )
 fig.show()
+
+
+import plotly.express as px
+import plotly.io as pio
+
+df3 = px.data.tips()
+df3
+df2 = df.reset_index()
+df2.head()
+
+ix = pd.IndexSlice
+df4 = df.loc[ix[:, 'confirmed'], '2020-04-05'].reset_index()
+
+fig = px.sunburst(
+    df4,
+    path=['status', 'Country/Region'],
+    values='2020-04-05')
+
+fig.update_traces(
+    go.Sunburst(
+        hovertemplate='<b>%{label} </b> <br> Cases: %{value:,.0f} ( %{percentEntry:.2%})</br>'),
+    insidetextorientation='radial',
+    textinfo='label+percent entry'
+)
+
+dct = fig.to_dict()
+
+
+
+fig = make_subplots(
+    rows=3, cols=3,
+    specs=[[{"rowspan": 2, "colspan": 3}, None, None],
+           [None, None, None],
+           [{"type": "domain"}, {"type": "domain"}, {"type": "domain"}]],
+    print_grid=True)
+
+fig.add_trace(dct['data'][0], row=3, col=1)
+
+dct['data'][0]
